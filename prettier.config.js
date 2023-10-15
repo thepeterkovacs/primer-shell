11b389db475b1c2107d07a44f7a00693c79acdc1
thepeterkovacs <peterkov37@gmail.com>
2023-10-15 10:23:55 +0200

    prettierignore and config

---
+++ prettier.config.js
@@ -0,0 +1,27 @@
+/** @type {import('prettier').Config} */
+export default {
+	arrowParens: "always",
+	bracketSameLine: true,
+	bracketSpacing: true,
+	endOfLine: "crlf",
+	importOrder: [
+		"^(react/(.*)$)|^(react$)",
+		"^(next/(.*)$)|^(next$)",
+		"^@/styles/(.*)$",
+		"^@/types/(.*)$",
+		"^@/lib/(.*)$",
+		"^@/database/(.*)$",
+		"^@/components/(.*)$",
+		"^@/app/(.*)$",
+		"^[./]",
+	],
+	importOrderSeparation: true,
+	importOrderSortSpecifiers: true,
+	plugins: ["@trivago/prettier-plugin-sort-imports"],
+	printWidth: 100,
+	semi: false,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "es5",
+	useTabs: true,
+}
