9d6ef09746efed472917ca8c9a08005a2781fd23
thepeterkovacs <peterkov37@gmail.com>
2023-10-15 16:09:05 +0200

    add validation function to input project name

--- src/commands/prompts/projectName.ts
+++ src/commands/prompts/projectName.ts
@@ -1,5 +1,7 @@
 import inquirer from "inquirer"
 
+import { validateProjectName } from "../../lib/validate.js"
+
 /**
  * Asynchronously prompts the user to input the project name and returns the entered name.
  * @returns {Promise<string>} A Promise that resolves with the entered project name.
@@ -7,7 +9,7 @@ import inquirer from "inquirer"
  * const projectName = await inputProjectName()
  */
 export default async function inputProjectName(): Promise<string> {
-	const answer: { projectName: string } = await inquirer.prompt([
+	const { projectName }: { projectName: string } = await inquirer.prompt([
 		{
 			name: "projectName",
 			type: "input",
@@ -15,5 +17,7 @@ export default async function inputProjectName(): Promise<string> {
 		},
 	])
 
-	return answer.projectName
+	validateProjectName(projectName)
+
+	return projectName
 }
