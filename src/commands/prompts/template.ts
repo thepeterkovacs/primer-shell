cf836731ecfd6f8d3f174f945d1bb95ea2677ef5
thepeterkovacs <peterkov37@gmail.com>
2023-10-15 14:37:21 +0200

    select template prompt

---
+++ src/commands/prompts/template.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer"
+
+/**
+ * Asynchronously prompts the user to select a template and returns the chosen one.
+ * @returns {Promise<string>} A Promise that resolves with the selected template.
+ * @example
+ * const template = await selectTemplate()
+ */
+export default async function selectTemplate(): Promise<string> {
+	const answer: { template: string } = await inquirer.prompt([
+		{
+			name: "template",
+			type: "list",
+			message: "Which template would you like to use?",
+			choices: [
+				{
+					name: "Web",
+				},
+			],
+		},
+	])
+
+	return answer.template
+}
