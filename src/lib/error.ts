b0714e050e6183b96fbddfcd12d10817ab9c85b1
thepeterkovacs <peterkov37@gmail.com>
2023-10-15 11:07:29 +0200

    add log error function

---
+++ src/lib/error.ts
@@ -0,0 +1,19 @@
+import chalk from "chalk"
+
+/**
+ * Logs an error message with a specified title and optional details to the console, then exits the process with an error code.
+ * @param {string} title Title of the error message.
+ * @param {unknown} [details] Optional details or additional information about the error.
+ * @returns {void}
+ * @example
+ * logError("Something went wrong")
+ */
+export default function logError(title: string, details?: unknown): void {
+	console.log(chalk.bgRed(title + "\n"))
+
+	if (details) {
+		console.log(chalk.red(details + "\n"))
+	}
+
+	process.exit(1)
+}
