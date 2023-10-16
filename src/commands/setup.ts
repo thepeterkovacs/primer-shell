8588bf5c1be0b0b1ec562f5531eef56c7805b48e
thepeterkovacs <peterkov37@gmail.com>
2023-10-16 19:02:42 +0200

    project setup function

---
+++ src/commands/setup.ts
@@ -0,0 +1,34 @@
+import cliSpinners from "cli-spinners"
+import { oraPromise } from "ora"
+
+import error from "../lib/error.js"
+import cloneRepository from "./git/clone.js"
+import { createPackageJson } from "./npm/package.js"
+import removeFiles from "./remove.js"
+
+/**
+ * Asynchronously sets up the project based on the specified template and project name.
+ * @param {string} template Template used for project setup.
+ * @param {string} projectName Name of the project to be set up.
+ * @returns {Promise<void>} A Promise that resolves once the project setup is completed successfully.
+ * @example
+ * await projectSetup("myTemplate", "myProjectName")
+ */
+export default async function projectSetup(template: string, projectName: string): Promise<void> {
+	try {
+		await oraPromise(setupProcess(template, projectName), {
+			text: "Setting up project...",
+			successText: "Project setup completed successfully",
+			spinner: cliSpinners.binary,
+			color: "cyan",
+		})
+	} catch (err) {
+		error("Project setup failed", err)
+	}
+}
+
+async function setupProcess(template: string, projectName: string) {
+	await cloneRepository(template, projectName)
+	await removeFiles(projectName)
+	await createPackageJson(template, projectName)
+}
