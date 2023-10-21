4b349178058108986940c2a5d06f8272d992ffe8
thepeterkovacs <peterkov37@gmail.com>
2023-10-21 14:12:34 +0200

    add config with const values

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
+import { APP_NAME } from "./config.js"
 import { inputProjectName, selectTemplate } from "./lib/commands/prompt.js"
 import projectSetup from "./lib/commands/setup.js"
 import showTitle from "./lib/commands/title.js"
 import { startVscode } from "./lib/commands/vscode.js"
 import { checkGitExists } from "./lib/utils/validation.js"
 
-await showTitle("Primer Shell", "Doom")
+await showTitle(APP_NAME, "Doom")
 
 const template = await selectTemplate()
 const projectName = await inputProjectName()
