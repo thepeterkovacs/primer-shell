e4dee2e295ed07f080a21a3cb697244a07e92929
thepeterkovacs <peterkov37@gmail.com>
2023-10-29 22:11:22 +0100

    create example router with procedures

---
+++ templates/web-primer-shell/src/server/api/routers/example.tsx
@@ -0,0 +1,30 @@
+import { z } from "zod"
+
+import { publicProcedure, router } from "../trpc"
+
+let exampleDataStorage = [
+	{
+		id: 1,
+		data: "existing_data",
+	},
+	{
+		id: 2,
+		data: "existing_data",
+	},
+]
+
+const getDataOutputType = z.array(z.object({ id: z.number(), data: z.string() }))
+
+const postDataInputType = z.object({ data: z.string() })
+
+export const exampleRouter = router({
+	getData: publicProcedure.output(getDataOutputType).query(async () => {
+		return exampleDataStorage
+	}),
+	postData: publicProcedure.input(postDataInputType).mutation(async ({ input }) => {
+		exampleDataStorage.push({
+			id: ++exampleDataStorage[exampleDataStorage.length - 1].id,
+			data: input.data,
+		})
+	}),
+})
