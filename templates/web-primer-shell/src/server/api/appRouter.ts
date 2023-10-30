005ab227155930046004d1a208738242f799cc80
thepeterkovacs <peterkov37@gmail.com>
2023-10-30 20:11:50 +0100

    example router to app router

--- templates/web-primer-shell/src/server/api/appRouter.ts
+++ templates/web-primer-shell/src/server/api/appRouter.ts
@@ -1,5 +1,8 @@
+import { exampleRouter } from "./routers/example"
 import { router } from "./trpc"
 
-export const appRouter = router({})
+export const appRouter = router({
+	example: exampleRouter,
+})
 
 export type AppRouter = typeof appRouter
