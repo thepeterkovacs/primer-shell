20fd6ff37a93164f9ace19027925e399b5241f62
thepeterkovacs <peterkov37@gmail.com>
2023-11-06 21:22:47 +0100

    change theme toggle component style

--- templates/web-primer-shell/src/components/tools/ThemeToggle.tsx
+++ templates/web-primer-shell/src/components/tools/ThemeToggle.tsx
@@ -11,9 +11,12 @@ export default function ThemeToggle(): JSX.Element {
 	const { theme, setTheme } = useTheme()
 
 	return (
-		<Button variant="ghost" onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
-			<SunIcon className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-			<MoonIcon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+		<Button
+			variant="ghost"
+			size="sm"
+			onClick={() => setTheme(theme === "light" ? "dark" : "light")}>
+			<SunIcon className="h-5 w-5 scale-100 dark:scale-0" />
+			<MoonIcon className="absolute h-5 w-5 scale-0 dark:scale-100" />
 			<span className="sr-only">Toggle theme</span>
 		</Button>
 	)
