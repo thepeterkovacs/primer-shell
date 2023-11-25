fb6caf6e6ee1910690433a3364cf9a2ae39765e1
thepeterkovacs <peterkov37@gmail.com>
2023-11-25 13:29:04 +0100

    change playwright config

--- templates/web-primer-shell/playwright.config.ts
+++ templates/web-primer-shell/playwright.config.ts
@@ -18,10 +18,6 @@ export default defineConfig({
 	reporter: [["list"], ["html", { outputFolder: "tests/export", open: "never" }]],
 	retries: process.env.CI ? 2 : 0,
 	testDir: "./tests",
-	use: {
-		baseURL: "http://127.0.0.1:3000",
-		trace: "on-first-retry",
-	},
 	webServer: {
 		command: "npm run dev",
 		url: "http://127.0.0.1:3000",
