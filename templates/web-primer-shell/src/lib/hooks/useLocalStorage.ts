27e5a07a74cda957fa047195a0472f263aa7f36a
thepeterkovacs <peterkov37@gmail.com>
2023-11-26 11:47:20 +0100

    rewriting hooks jsdoc

--- templates/web-primer-shell/src/lib/hooks/useLocalStorage.ts
+++ templates/web-primer-shell/src/lib/hooks/useLocalStorage.ts
@@ -1,15 +1,12 @@
 import { Dispatch, useEffect, useState } from "react"
 
 /**
- * Custom hook to manage state in localStorage.
- * @param {string} key Key to be used for storing and retrieving the data in localStorage.
- * @param {any} defaultValue Default value to be used if the key doesn't exist in localStorage.
- * @returns {[any, Dispatch<any>]} A tuple containing the current value stored in localStorage and a function to update it.
+ * Manages a state in the local storage.
+ * @param {string} key Key to be used for storing and retrieving data from the local storage.
+ * @param {any} defaultValue Default value to be used if the key does not exist in the local storage.
+ * @returns {[any, Dispatch<any>]} A tuple containing the current value stored in the local storage and a function to update it.
  * @example
- * const [output, setOutput] = useLocalStorage("output", 0)
- * //output = 0
- * setOutput(2)
- * //output = 2
+ * const [data, setData] = useLocalStorage("data", 0)
  */
 const useLocalStorage = (key: string, defaultValue: any): [any, Dispatch<any>] => {
 	const [localItem, setLocalItem] = useState<[any, Dispatch<any>]>()
