d763a45bba2bd774fb00c2f96e46b905d5a7eeb7
thepeterkovacs <peterkov37@gmail.com>
2023-11-26 11:55:27 +0100

    revert to inter font

--- templates/web-primer-shell/src/lib/assets/fonts.ts
+++ templates/web-primer-shell/src/lib/assets/fonts.ts
@@ -1,6 +1,6 @@
-import { Raleway } from "next/font/google"
+import { Inter } from "next/font/google"
 
-export const raleway = Raleway({
+export const inter = Inter({
 	subsets: ["latin"],
 	weight: "400",
 })
