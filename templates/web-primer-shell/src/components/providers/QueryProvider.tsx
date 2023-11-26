af0e56f21583d16e0f592e58b1729aeaefd2bf14
thepeterkovacs <peterkov37@gmail.com>
2023-11-26 19:02:50 +0100

    trpc client options to query provider

--- templates/web-primer-shell/src/components/providers/QueryProvider.tsx
+++ templates/web-primer-shell/src/components/providers/QueryProvider.tsx
@@ -1,23 +1,50 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { httpBatchLink } from "@trpc/client"
+import { loggerLink, unstable_httpBatchStreamLink } from "@trpc/client"
+import { PropsWithChildren, useState } from "react"
 
-import React, { ReactNode, useState } from "react"
+import trpcClient from "_trpc/client"
 
-import { trpcClient } from "@/lib/utils/trpc/client"
+import { getTrpcUrl, isDevEnv } from "utils/standard"
 
-export default function QueryProvider({ children }: { children: ReactNode }): JSX.Element {
-	const [queryClient] = useState(() => new QueryClient({}))
+interface Props extends PropsWithChildren {
+	headers: Headers
+}
+
+export default function QueryProvider({ children, headers }: Props): JSX.Element {
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnMount: false,
+						refetchOnReconnect: false,
+						refetchOnWindowFocus: false,
+						retry: false,
+					},
+				},
+			}),
+	)
 
 	const [client] = useState(() =>
 		trpcClient.createClient({
 			links: [
-				httpBatchLink({
-					url: `${process.env.NEXT_PUBLIC_URL}/api/trpc`,
+				loggerLink({
+					enabled: (opts) =>
+						(isDevEnv() && typeof window !== "undefined") ||
+						(opts.direction === "down" && opts.result instanceof Error),
+				}),
+				unstable_httpBatchStreamLink({
+					url: getTrpcUrl(),
+					headers() {
+						const requestHeaders = new Map(headers)
+						requestHeaders.set("x-trpc-source", "client")
+						return Object.fromEntries(requestHeaders)
+					},
 				}),
 			],
-		})
+		}),
 	)
 
 	return (
