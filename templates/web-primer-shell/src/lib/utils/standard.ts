528432d3c34d28af9e96197269055a22b5f1d24e
thepeterkovacs <peterkov37@gmail.com>
2023-11-26 11:54:57 +0100

    update standard utils with new functions

--- templates/web-primer-shell/src/lib/utils/standard.ts
+++ templates/web-primer-shell/src/lib/utils/standard.ts
@@ -12,7 +12,51 @@ import { twMerge } from "tailwind-merge"
 export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs))
 
 /**
+ * Retrieves the public URL of the application using the `NEXT_PUBLIC_URL` environment variable.
+ * @returns {string} Public URL of the application.
+ * @example
+ * const appUrl = getAppUrl();
+ */
+export const getAppUrl = (): string => process.env.NEXT_PUBLIC_URL
+
+/**
+ * Retrieves the tRPC API URL of the application.
+ * @returns {string} tRPC URL of the application.
+ * @example
+ * const trpcUrl = getTrpcUrl();
+ */
+export const getTrpcUrl = (): string => `${process.env.NEXT_PUBLIC_URL}/api/trpc`
+
+/**
+ * Checks if the application is running in development environment.
+ * @returns {boolean} `true` if the environment is development, `false` otherwise.
+ * @example
+ * if (isDevEnv()) console.log("development")
+ */
+export const isDevEnv = (): boolean => process.env.NODE_ENV === "development"
+
+/**
+ * Checks if the application is running in production environment.
+ * @returns {boolean} `true` if the environment is production, `false` otherwise.
+ * @example
+ * if (isProdEnv()) console.log("production")
+ */
+export const isProdEnv = (): boolean => process.env.NODE_ENV === "production"
+
+/**
+ * Delays the execution by a specified number of milliseconds.
+ * @async
+ * @param {number} ms Number of milliseconds to wait.
+ * @returns {Promise<void>} A Promise that resolves after the specified delay.
+ * @example
+ * await wait(1000);
+ */
+export const wait = async (ms: number): Promise<void> =>
+	await new Promise((resolve) => setTimeout(resolve, ms))
+
+/**
  * Fetches the HTML content from a specified URL.
+ * @async
  * @param {string} url The URL from which to fetch HTML content.
  * @returns {Promise<string>} A Promise that resolves with the fetched HTML content as a string.
  * @example
@@ -21,23 +65,4 @@ export const cn = (...inputs: ClassValue[]): string => twMerge(clsx(inputs))
 export const getHtmlFromUrl = async (url: string): Promise<string> => {
 	const res = await fetch(url, { cache: "no-store" })
 	return await res.text()
-}
-
-/**
- * Extracts a substring from a string, given a prefix and suffix.
- * @param {string} str The string to extract the substring from.
- * @param {string} prefix Prefix of the substring to extract.
- * @param {string} suffix Suffix of the substring to extract.
- * @returns {string} The substring extracted from the string, or an empty string if the substring could not be found.
- * @example
- * const extracted = extractSubstring("Some string as str.", "Some ", " as str.")
- * //extracted = "string"
- */
-export const extractSubstring = (str: string, prefix: string, suffix: string): string => {
-	const startIndex = str.indexOf(prefix)
-	const endIndex = str.indexOf(suffix)
-
-	const subString = str.slice(startIndex + prefix.length, endIndex)
-
-	return subString
 }
