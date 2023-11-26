7a4da0a0d132110f00a0b90a0638268cb7a292f8
thepeterkovacs <peterkov37@gmail.com>
2023-11-26 11:57:36 +0100

    redesign trpc core with context and middleware

--- templates/web-primer-shell/src/server/api/trpc.ts
+++ templates/web-primer-shell/src/server/api/trpc.ts
@@ -1,12 +1,64 @@
-import { initTRPC } from "@trpc/server"
+import { TRPCError, inferRouterInputs, inferRouterOutputs, initTRPC } from "@trpc/server"
+import { Session } from "next-auth"
+import { NextRequest } from "next/server"
+import { ZodError } from "zod"
 
-const t = initTRPC.create()
+import { RootRouter } from "api/root"
 
-export const authProcedure = t.middleware(({ next }) => {
-	return next()
+import { getServerAuthSession } from "auth/session"
+
+export interface Context {
+	headers: Headers | null
+	session: Session | null
+}
+
+/**
+ * Data that all procedures have access to.
+ */
+export const context = async (opts: { req: NextRequest }): Promise<Context> => {
+	const session = await getServerAuthSession()
+
+	return {
+		headers: opts.req.headers,
+		session,
+	}
+}
+
+const trpc = initTRPC.context<Context>().create({
+	errorFormatter({ shape, error }) {
+		return {
+			...shape,
+			data: {
+				...shape.data,
+				zodError: error.cause instanceof ZodError ? error.cause.flatten() : null,
+			},
+		}
+	},
 })
 
-export const router = t.router
+export const router = trpc.router
 
-export const publicProcedure = t.procedure
-export const privateProcedure = t.procedure.use(authProcedure)
+/**
+ * Publicly available procedure without any authentication required.
+ */
+export const publicProcedure = trpc.procedure
+
+/**
+ * Procedure only available with an existing session.
+ */
+export const privateProcedure = trpc.procedure.use(
+	trpc.middleware(({ ctx, next }) => {
+		if (!ctx.session || !ctx.session.user) {
+			throw new TRPCError({ code: "UNAUTHORIZED" })
+		}
+
+		return next({
+			ctx: {
+				session: { ...ctx.session, user: ctx.session.user },
+			},
+		})
+	}),
+)
+
+export type RouterInputs = inferRouterInputs<RootRouter>
+export type RouterOutputs = inferRouterOutputs<RootRouter>
